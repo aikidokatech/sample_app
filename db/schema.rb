3845fb28746f9386542c49daf518b2d4a39c0110
Adam Oliver <aikidokatech@gmail.com>
2012-04-01 13:27:20 -0400

    Finished HW7

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,13 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120318163306) do
+ActiveRecord::Schema.define(:version => 20120331202652) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+    t.string   "encrypted_password"
+    t.string   "salt"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
---
+++ spec/factories.rb~
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name			"Adam Oliver"
+  user.email			"me@me.com"
+  user.password			"foobar"
+  user.password_confirmation	"foobar"
+end
